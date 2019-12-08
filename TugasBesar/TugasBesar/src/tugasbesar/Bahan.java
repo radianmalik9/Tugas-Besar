63dc4d72cd7074bbbf76ef8f75b8abfd583a61d7
radianmalik9 <radianmalik9@gmail.com>
2019-12-08 14:00:57 +0700

    form lanjutan

---
+++ TugasBesar/TugasBesar/src/tugasbesar/Bahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasbesar;
+
+/**
+ *
+ * @author user
+ */
+public class Bahan {
+    private String textur;
+    
+    Bahan(){
+        
+    }
+    Bahan(String textur){
+        this.textur = textur;
+    }
+
+    public String getTextur() {
+        return textur;
+    }
+
+    public void setTextur(String textur) {
+        this.textur = textur;
+    }
+    public void info(){
+        System.out.printf("Bahan = %s\n", textur);
+    }
+}
